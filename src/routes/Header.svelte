84ff235b6f1f127765beee607fd078407037f394
Hadi Khir <hadi.khir@sap.com>
2023-05-04 23:35:25 -0400

    adding linkedin link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,9 +1,16 @@
 <script>
 	import { page } from '$app/stores';
+	import linkedin from '$lib/images/github.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
+	<div class="corner">
+		<a href="https://www.linkedin.com/in/hadi-khir-a14338179/">
+			<img src={linkedin} alt="SvelteKit" />
+		</a>
+	</div>
+
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
